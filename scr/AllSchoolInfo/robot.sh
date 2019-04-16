454b5c4646200293e9b472613145c17a7e50da3c
QingQiz <sofeeys@outlook.com>
2019-04-16 22:16:36 +0800

    get school information from icpc

---
+++ scr/AllSchoolInfo/robot.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+auth=$( cat ./auth )
+
+main() {
+    cmd="curl 'https://icpc.baylor.edu/cm5-common-rest/rest/common/institutionunit/suggest?name=$1&page=0&size=10000' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-GB,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: application/json' -H 'Referer: https://icpc.baylor.edu/private/profile/404758' -H 'Authorization: $auth' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed"
+}
+
+search() {
+    res=$( eval ${cmd} )
+    jud=$( echo ${res} | grep -i error )
+
+    if [[ ${jud} == '' ]]; then
+        echo ${res} > ./res/'result'$1'.txt'
+        echo ${res}
+    else
+        exit 1
+    fi
+}
+
+for xx in `cat ./dict`; do
+    main
+    search ${xx}&
+done
