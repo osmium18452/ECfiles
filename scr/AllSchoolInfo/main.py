9ce60786459e1927794e8f147c2e680e7dccba88
QingQiz <sofeeys@outlook.com>
2019-11-02 18:11:34 +0800

    重写了学校信息的爬虫, 添加了readme 添加了爬虫结果 (以后信息可能会有更新, 需要重新使用爬虫爬去)

--- scr/AllSchoolInfo/main.py
+++ scr/AllSchoolInfo/main.py
@@ -4,86 +4,60 @@ import os
 import json
 import functools
 import subprocess
-import selenium.common
 from time import sleep
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
-os.system('killall phantomjs')
-os.system('mkdir -p ./res')
-with open('./dict', 'w') as f:
-    for i in "qwertyuiopasdfghjklzxcvbnm":
-        f.write(i + '\n')
+username = ''
+password = ''
 
-service_args = [
-    '--proxy=localhost:1080',
-    '--proxy-type=socks5',
-]
-d = webdriver.PhantomJS(service_args=service_args)
-d.get('https://icpc.baylor.edu/')
 
-while True:
-    try:
-        d.find_element_by_xpath('//*[contains(text(), "Login")]').click()
-        break
-    except selenium.common.exceptions.NoSuchElementException:
-        sleep(1)
-while True:
-    try:
-        d.find_element_by_id('username').send_keys('642191352@qq.com')
-        d.find_element_by_id('password').send_keys('zxcvbnm1234567')
-        d.find_element_by_class_name('button').click()
-        break
-    except selenium.common.exceptions.NoSuchElementException:
-        sleep(1)
-
-while True:
-    try:
-        d.find_element_by_xpath('//*[contains(text(), "My Dashboard")]')
-        break
-    except selenium.common.exceptions.NoSuchElementException:
-        sleep(1)
+def waiting_for(d, elem):
+    while True:
+        try:
+            d.find_element_by_xpath(elem)
+            break
+        except NoSuchElementException:
+            sleep(1)
+    return d.find_element_by_xpath(elem)
 
 
 def get_auth():
-    auth = ""
+    service_args = [
+        '--proxy=localhost:1080',
+        '--proxy-type=socks5',
+    ]
+    os.system('killall phantomjs')
+    d = webdriver.PhantomJS(service_args=service_args)
+    d.implicitly_wait(60)
+    d.get('https://icpc.baylor.edu/')
+    waiting_for(d, '//*[contains(text(), "Login")]').click()
+    d.find_element_by_id('username').send_keys(username)
+    d.find_element_by_id('password').send_keys(password)
+    d.find_element_by_class_name('button').click()
+
+    waiting_for(d, '//*[contains(text(), "My Dashboard")]')
     log = json.loads(d.get_log('har')[0].get('message')).get('log').get('entries')
-    for r in reversed(log):
-        flg = False
-        for i in r.get('request').get('headers'):
-            if i.get('name') == "Authorization":
-                auth = i.get('value')
-                flg = True
-                break
-        if flg:
+    d.close()
+
+    for r in log[::-1]:
+        for header in r.get('request').get('headers'):
+            if header.get('name') == "Authorization":
+                with open('./auth', 'w') as f:
+                    print(header.get('value'), file=f, end='')
+                return
+
+
+if __name__ == '__main__':
+    get_auth()
+    res = subprocess.call(["./robot.sh", ], shell=True)
+    while True:
+        if len(os.listdir('./res')) == 26:
+            res = list({j['id']: j for f in os.listdir('./res') for j in json.load(open('./res/' + f))}.values())
+            res.sort(key=functools.cmp_to_key(lambda a, b: a['id'] - b['id']))
+            with open('result.json', 'w') as f:
+                json.dump(res, f, ensure_ascii=False, indent=4)
             break
-    with open('./auth', 'w') as f:
-        f.write(auth.strip('\n'))
-
-
-get_auth()
-res = subprocess.call(["./robot.sh", ], shell=True)
-
-while True:
-    if len(os.listdir('./res')) == 26:
-        direct = {}
-        for f in os.listdir('./res'):
-            if f[-3:] != 'txt':
-                continue
-            js = json.load(open('./res/' + f))
-            for obj in js:
-                direct[obj['id']] = obj
-        l = []
-        for i in direct:
-            l.append(direct[i])
-        l.sort(key=functools.cmp_to_key(lambda a, b: a['id'] - b['id']))
-
-        with open('result.json', 'w') as f:
-            json.dump(l, f, ensure_ascii=False, indent=4)
-        d.close()
-        break
-    print("waiting for robot...")
-    sleep(3)
-
-os.system('rm ./auth')
-os.system('rm -rf ./res')
-os.system('rm ./dict')
+        print("waiting for robot...")
+        sleep(3)
+    os.system('rm -rf ./auth ./dict ./res ./ghostdriver.log')
--- scr/AllSchoolInfo/robot.sh
+++ scr/AllSchoolInfo/robot.sh
@@ -2,8 +2,10 @@
 
 auth=$( cat ./auth )
 
+mkdir -p ./res
+
 main() {
-    cmd="curl 'https://icpc.baylor.edu/cm5-common-rest/rest/common/institutionunit/suggest?name=$1&page=0&size=10000' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-GB,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: application/json' -H 'Referer: https://icpc.baylor.edu/private/profile/404758' -H 'Authorization: $auth' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed"
+    cmd="curl --proxy socks5://127.0.0.1:1080 'https://icpc.baylor.edu/cm5-common-rest/rest/common/institutionunit/suggest?name=$1&page=0&size=10000' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-GB,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' -H 'Accept: application/json' -H 'Referer: https://icpc.baylor.edu/private/profile/404758' -H 'Authorization: $auth' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed"
 }
 
 search() {
@@ -18,7 +20,8 @@ search() {
     fi
 }
 
-for xx in `cat ./dict`; do
+dict=(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+for xx in ${dict[@]}; do
     main
     search ${xx}&
 done
