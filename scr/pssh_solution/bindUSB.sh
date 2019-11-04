185834e7814e4d7e33e0fae100db6e6cc7d06cbd
QingQiz <sofeeys@outlook.com>
2019-11-04 15:26:21 +0800

    remove autogetip.sh, move storage to storage.bak

--- scr/pssh_solution/bindUSB.sh
+++ scr/pssh_solution/bindUSB.sh
@@ -4,7 +4,7 @@ dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 cd $dir/../..
 
-command='mkdir /etc/bak -p && mv /lib/modules/`uname -r`/kernel/drivers/usb/storage/ /etc/bak'
+command='mkdir /etc/bak -p && mv /lib/modules/`uname -r`/kernel/drivers/usb/storage/ /lib/modules/`uname -r`/kernel/drivers/usb/storage.bak'
 
 parallel-ssh -t 1 -h ./iplist -v $command
 parallel-ssh -t 1 -h ./iplist -v "chmod 000 /media -R"
